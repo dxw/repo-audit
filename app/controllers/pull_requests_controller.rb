1ab1cce56f8b0ca72786ddddfdd602dfdea9b02a
Cristina <cristina@dxw.com>
2020-08-18 12:23:57 +0100

    Catch errors when attempting to create a PR
    It is possible to try creating a PR for a repo that already has one. In that case, the ref is the same, and the creation attempt fails with `Octokit::UnprocessableEntity: 422 - Reference already exists`.

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -2,6 +2,13 @@ class PullRequestsController < ApplicationController
   def create
     pull_request = PullRequestCreator.new(params[:repo_name]).open_pull_request
     # TODO remove
-    logger.info("Created pull request #{pull_request}")
+    logger.info("Created pull request #{pull_request.inspect}")
+
+    flash[:info] = "Created pull request #{pull_request.inspect}"
+  rescue StandardError => e
+    # TODO remove?
+    logger.error("Error creating pull request: #{e.messages}")
+
+    flash[:error] = "Encountered an error creating the pull request. The error was #{e.messages}"
   end
 end
